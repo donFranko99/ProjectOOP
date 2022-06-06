e73dd203805f59c07678b7552e7a74c5bdba5887
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2022-06-06 15:55:27 +0200

    Created classes for storing DB data

--- WaterRescueInterventionRegister/WaterRescueDBConversion/DataFromDB.cs
+++ WaterRescueInterventionRegister/WaterRescueDBConversion/DataFromDB.cs
@@ -2,7 +2,31 @@ using System;
 
 namespace WaterRescueDBConversion
 {
-    public class DataFromDB
+    public class Lifeguard
     {
+        public int LifeguardID { get; set; }
+        public string LifeguardName { get; set; }
+        public string LifeguardSurname { get; set; }
+        public char[] LifeguardPhoneNumber { get; set; }
+        public int LifeguardRole { get; set; }
+    }
+    public class Report
+    {
+        public int InterventionID { get; set; }
+        public DateOnly InterventionDate { get; set; }
+        public TimeOnly InterventionTime { get; set; }
+        public string InterventionReport { get; set; }
+    }
+    public class Intervention
+    {
+        public int InterventionID { get; set; }
+        public int LifeguardID { get; set; }
+        public DateOnly InterventionDate { get; set; }
+        public int ResponseTime { get; set; }
+    }
+    public class Role
+    {
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
     }
 }
